bf5247e9d8efcfae13097f51953828371f19672f
joaopedroAnelli <joaopedroanelli99@gmail.com>
2022-01-26 16:08:01 -0300

    Outra versão

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Olá Docker, v3.0.0!');
+  res.send('Olá Docker, v3.0.1!');
 });
 
 app.listen(3000);
